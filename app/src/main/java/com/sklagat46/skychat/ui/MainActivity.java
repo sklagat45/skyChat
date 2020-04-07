6b2d195199401285ee98641f02d89bf96a4f89aa
sklagat45 <sklagat46@gmail.com>
2020-04-07 23:32:53 +0300

    resolved register and login errors

--- app/src/main/java/com/sklagat46/skychat/ui/MainActivity.java
+++ app/src/main/java/com/sklagat46/skychat/ui/MainActivity.java
@@ -1,16 +1,63 @@
 package com.sklagat46.skychat.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.sklagat46.skychat.R;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        mToolbar = (Toolbar)findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("SkyChat");
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
+        if(currentUser == null){
+            sendToStart();
+        }
+    }
+
+    private void sendToStart() {
+        Intent signin_Intent = new Intent(MainActivity.this, SignInActivity.class);
+        startActivity(signin_Intent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+         super.onOptionsItemSelected(item);
+         if(item.getItemId() == R.id.mainLogoutBtn){
+             FirebaseAuth.getInstance().signOut();
+             sendToStart();
+         }
+         return true;
     }
 }
